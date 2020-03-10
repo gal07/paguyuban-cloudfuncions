0b56eec2e5e74205f5e29dca0a528f2fa13497c3
Galih <galihkur29@gmail.com>
2020-03-10 17:56:38 +0700

    first commit

---
+++ functions/src/index.ts
@@ -0,0 +1,102 @@
+import * as functions from 'firebase-functions';
+import * as firebase from 'firebase-admin';
+import * as serviceAccount from './service';
+
+const params = {
+  type: serviceAccount.service.type,
+  projectId: serviceAccount.service.project_id,
+  privateKeyId: serviceAccount.service.private_key_id,
+  privateKey: serviceAccount.service.private_key,
+  clientEmail: serviceAccount.service.client_email,
+  clientId: serviceAccount.service.client_id,
+  authUri: serviceAccount.service.auth_uri,
+  tokenUri: serviceAccount.service.token_uri,
+  authProviderX509CertUrl: serviceAccount.service.auth_provider_x509_cert_url,
+  clientC509CertUrl: serviceAccount.service.client_x509_cert_url
+}
+   
+firebase.initializeApp({
+  credential: firebase.credential.cert(params),
+})
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+export const usermanagement = functions.https.onRequest((request, response) => {
+  // response.send({request:request.body})
+  /** 
+   * Create User For Member
+   */
+  if (request.body.type == 1) {
+    firebase.auth().createUser({
+      email: request.body.data.email.toLowerCase(),
+      emailVerified: true,
+      password: request.body.data.email,
+      displayName: request.body.data.nama,
+      disabled: false
+  }).then(function(result) {
+      // See the UserRecord reference doc for the contents of userRecord.
+      console.log('Successfully created new user:', result.uid+' console : '+request.headers["content-type"]);
+      response.send({success:1,name:result.displayName,uid:result.uid});
+    })
+    .catch(function(error) {
+      console.log('Error creating new user:', error);
+      response.send({error:error,success:0});
+    });
+    /**
+     * Create User For Guru
+     */
+  }else if(request.body.type == 2){
+      firebase.auth().createUser({
+        email: request.body.data.email.toLowerCase(),
+        emailVerified: true,
+        password: request.body.data.nip,
+        displayName: request.body.data.name,
+        disabled: false
+    }).then(function(result) {
+        // See the UserRecord reference doc for the contents of userRecord.
+        console.log('Successfully created new user:', result.uid+' console : '+request.headers["content-type"]);
+        response.send({success:1,name:result.displayName,uid:result.uid});
+      })
+      .catch(function(error) {
+        console.log('Error creating new user:', error);
+        response.send({error:error,success:0});
+      });
+      /**
+       * Delete User
+       */
+  }else if(request.body.type == 3){
+      firebase.auth().deleteUser(request.body.key).then(function(result){
+        console.log(result);
+        response.send({success:1});
+      }).catch(function(error){
+        console.log(error);
+        response.send({success:0});
+      })
+  }else if(request.body.type == 4){
+      firebase.auth().createUser({
+        email: request.body.data.email_sebagai.toLowerCase(),
+        emailVerified: true,
+        password: request.body.data.nisn,
+        displayName: request.body.data.nama_sebagai,
+        phoneNumber:"+62"+request.body.data.telp_sebagai.substring(1),
+        disabled: false
+    }).then(function(result) {
+        // See the UserRecord reference doc for the contents of userRecord.
+        console.log('Successfully created new user:', result.uid+' console : '+request.headers["content-type"]);
+        response.send({success:1,name:result.displayName,uid:result.uid});
+      })
+      .catch(function(error) {
+        console.log('Error creating new user:', error);
+        response.send({error:error,success:0});
+      });
+  }else if(request.body.type == 5){
+    firebase.auth().generatePasswordResetLink(request.body.data.email).then(function(result){
+      console.log('Successfully send reset link password')
+      response.send({success:1,msg:'Successfully send reset link password, Check Your Email'});
+    }).catch(function(error){
+      console.log({error:error,success:0})
+      response.send({error:error,success:0})
+    })
+  }
+
+});
---
+++ functions/src/service.ts
@@ -0,0 +1,14 @@
+export const service = {
+
+    type: "service_account",
+    project_id: "paguyuban-52264",
+    private_key_id: "2642b3c39e60ac8e696bd4606fcdbf5c8f553a53",
+    private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDaN9i8Y4zZVy96\nU+MGn3SfpZw12i6RBWh0j7h57uaqtXHFvCHJ43zfTVkl7gt8k7olEe8Ln6Ix5Q4h\nTEf88FEXn0Jrc1LS+QH6zZtDwCIDWvElnzO8V7jcVnF+8UTAVZRF1PUvFZoYrO0d\n/C1Vc1jTbxr2Zf7yB3SsPZR8Daa5fXE7wIzTsf5peZI3hm0/B3vYGWlRbawBaknk\nYjz4c8tUd/fRmCvnuKy4C9UtyaZoEvgg7iWyFOq84lBz3NGMdSAZMnYoRbpDPYKU\nlXyZv2Pi2YnMPM1WVtZC1cZPuxjpoaDcVoQGnbzE6eQOylEkhUSpyAC+M1utNYiR\n4IT+VPA/AgMBAAECggEAFsJzCoMKMl4+MhtZtwZz2srNFhjMI+KqgwMSZzVG6FBw\nWK1HNjqnJYB4ZypYFwWECVBrhuIpaobMsyW07xP+1os3xDk3pDrFvWuAYXqX5uhZ\n9RCRDCMiFr5gAVtso5rJZ4HPPg/vYy92CQXqPIVWowbC4HwW28DHUceXxbCEI8hY\n8SryEyfn1/3TpH+Jw8tNm0LbEqiQIC0kqUZAUNT2jAMyRS0YPnJkbSrtsFdWkCjy\nMzRp+UmIiG5nuNdOF/v0Tu72IIVROmodrckbA7wcwulEyDPvmcP3lOkCtri1bulE\nGbZIXAA+3Skav8NcdLQDP7M/iNcQDA/By+3N+jKtwQKBgQDyDvWo9+MJ4MRjNgHj\nOQY+iwfJigzFQyzrLAc+L94vylvf67ohYUhPsLhNce+ayIlN1kcw6q/y9CejJ4fk\nY3TFhs/eSuz9N5+pC+dooLk+RLcar5yH8hMiYR4tzpdSXafXt90t5gycMiNNOabp\nEQlzQxvGlAWIvL2LsyUCwFGyLwKBgQDmyV961pDYuSI1fErnB8mbiQaAL+jTClh0\nRvCyELhJbqEWwqkuEny9bDjujj8CIQqwGoPoDmXS1RQOxxZe6tp9q0GFO5KmPAE4\nV5zlE2X06olQns8RqyQEck+3eaVa8pgfK+zWTclsX9dyOLUwMdPbphk+FNvR359X\n12G56+Ru8QKBgDo9Dy4QNjfyNE4KeHnLBo2VtdHvvRLZDr2mYje8HKEy9tfxR18w\noM1Ln4xClXuQNDjeDVLfpGW4CLFgmXmB1F6SCQFa1Zo2/9sUWiD6SpeIwOVSyz6K\nJ8BlzXUH97CBiBiGzFh2uSH7jjLOZDI/Px7UTqKAirRTdOdfus/mpcFRAoGBAKiA\nmu/rPXKfKXT3vRCVxQOEtnRdhpaf1RmAujiosIUc+wzNMAztfponLCIxRZveWBlg\nIrMJ4BphbPWlMpY2zzQ2F+6ZImexJArpHSq8URLTM1LjwLhbxj5g4AdMEAkpWxxX\nS+LkhI5WI83HzbLUjqSuz3tea2j+5y6RmU7QBsdhAoGACSRF5n2ojEFoTzrC5Jb1\nMNrhq8JB4aF49ExRTDGTI5fR0M1eop+yfS9ZiBlqFhebQ7UcdQ4nLkn055sJO2Zi\nIo1RKQp60IM0n3h3KZTsgLG+GEp294lN2vcHmawKDLn9eMz3cUxOdBpsimm+I8NW\n4me2nrlTOdM1KJ+yRaCp8sw=\n-----END PRIVATE KEY-----\n",
+    client_email: "paguyuban-52264@appspot.gserviceaccount.com",
+    client_id: "109448244307613990522",
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/paguyuban-52264%40appspot.gserviceaccount.com"
+    
+}
